349116173f486b4ef055732ce9861cf94ba3e775
Oloishorua <jacoboloishorua@gmail.com>
2024-08-30 00:10:54 +0300

    task 0 - function that log message obfuscated using regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module level functions for logging purposes"""
+
+
+from typing import List
+import re
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
