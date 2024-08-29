1c8eb9691ee57a08d0ca56dc5f7477eb708d7945
Oloishorua <jacoboloishorua@gmail.com>
2024-08-30 00:31:30 +0300

    task 6 - Checks if provided paswd matches hashed paswd

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password which is a byte string
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if provided paswd matches hashed paswd
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
