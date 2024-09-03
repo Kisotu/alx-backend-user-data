b122c55c29cc200fad48cac71797ec4f59404d5f
Oloishorua <jacoboloishorua@gmail.com>
2024-09-03 22:37:52 +0300

    setup files

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,39 +1,24 @@
 #!/usr/bin/env python3
-"""Route module for the API.
 """
-import os
+Route module for the API
+"""
 from os import getenv
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-
-from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
+import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
-    auth = Auth()
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler.
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler.
-    """
-    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
