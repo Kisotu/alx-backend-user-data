fa3670c004510914bdcee102a0deeefa1d884381
Oloishorua <jacoboloishorua@gmail.com>
2024-09-05 21:03:55 +0300

    updated users.app file

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,12 +27,12 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    user = User.get(user_id)
     if user_id == 'me':
         if request.current_user is None:
             abort(404)
         else:
             return jsonify(request.current_user.to_json())
+    user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())
