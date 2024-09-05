533beda0422d2c7d29bf61bc05e9dcf6a7185bca
Oloishorua <jacoboloishorua@gmail.com>
2024-09-05 22:35:25 +0300

    creates a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""Session authentication module for the API.
+"""Session authentication for the API.
 """
+from uuid import uuid4
 from flask import request
 
 from .auth import Auth
@@ -9,4 +10,19 @@ from .auth import Auth
 class SessionAuth(Auth):
     """Session authentication class.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates user session id
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Gets the user id for the user associated with
+        a given session id.
+        """
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
