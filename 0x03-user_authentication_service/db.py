6ffb6abd7de6c86116887f2f32ece8a28c05f194
Oloishorua <jacoboloishorua@gmail.com>
2024-09-13 00:25:01 +0300

    task 2 - function that finds user based on filtered args

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,10 +2,12 @@
 
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,4 +39,22 @@ class DB:
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
+
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on filtered args
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
