710478e372d523f783ff95158f6d4d324ff3fe9e
Oloishorua <jacoboloishorua@gmail.com>
2024-09-13 01:50:19 +0300

    task 12 - retrieves user based on session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@
 import bcrypt
 from uuid import uuid4
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
 
 from db import DB
 from user import User
@@ -67,3 +68,15 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Gets from db user based on given session ID.
+        """
+        user = None
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
