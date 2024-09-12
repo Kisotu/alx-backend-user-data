4e4e710a037baee5666096d18193d368f8fd1031
Oloishorua <jacoboloishorua@gmail.com>
2024-09-13 01:20:04 +0300

    task 7 - added a POST /users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """A basic Flask app with routes for auth testing
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from auth import Auth
 
@@ -17,6 +17,20 @@ def index() -> str:
         - The payload for the home page
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """POST /users
+    Return:
+        - payload after creating user account
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
