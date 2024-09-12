d48722652804dfa4d03fffe7c7c9db020b6b76fc
Oloishorua <jacoboloishorua@gmail.com>
2024-09-13 00:15:03 +0300

    task 1 - removed try catch block

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,11 +34,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """saves a user to the database.
         """
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            new_user = None
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
